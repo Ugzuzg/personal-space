30ef35a32395c706e852818c841a43df88fbf072
Jaryk <ugzuzg@gmail.com>
2025-05-09 20:19:48 +0200

    turn everything into an article (#9)

--- website/app/components/Completion.tsx
+++ website/app/components/Completion.tsx
@@ -6,41 +6,47 @@ export function Completion({ data }) {
   const allBoulders = data.map((v) => v.all).reduce((a, b) => a + b, 0);
 
   return (
-    <>
-      {`${allSent} / ${allBoulders} (${Number(allSent / allBoulders).toLocaleString(undefined, { style: 'percent', minimumFractionDigits: 2 })})`}
-      <PlotFigure
-        width={400}
-        options={{
-          figure: true,
-          grid: true,
-          width: 400,
-          marginLeft: 80,
-          marginBottom: 50,
-          color: { scheme: 'YlGn' },
-          style: {
-            fontFamily: 'Inter',
-            fontSize: 'var(--step--1)',
-          },
-          marks: [
-            Plot.frame(),
-            Plot.barX(data, {
-              y: 'difficulty',
-              x: 'all',
-              title: (v) =>
-                `${v.sent} / ${v.all} (${Number(v.completion).toLocaleString(undefined, { style: 'percent' })})`,
-              fill: 'rgba(169, 169, 169, 0.5)',
-            }),
-            Plot.barX(data, {
-              y: 'difficulty',
-              x: 'sent',
-              title: (v) =>
-                `${v.sent} / ${v.all} (${Number(v.completion).toLocaleString(undefined, { style: 'percent' })})`,
-              fill: 1,
-            }),
-          ],
-          y: { type: 'band', reverse: true },
-        }}
-      />
-    </>
+    <article>
+      <h2>Boulder progress at Klättercentret Solna</h2>
+      <p>
+        Overall progress:{' '}
+        {`${allSent} / ${allBoulders} (${Number(allSent / allBoulders).toLocaleString(undefined, { style: 'percent', minimumFractionDigits: 2 })})`}
+      </p>
+      <p>
+        <PlotFigure
+          width={400}
+          options={{
+            figure: true,
+            grid: true,
+            width: 400,
+            marginLeft: 80,
+            marginBottom: 50,
+            color: { scheme: 'YlGn' },
+            style: {
+              fontFamily: 'Inter',
+              fontSize: 'var(--step--1)',
+            },
+            marks: [
+              Plot.frame(),
+              Plot.barX(data, {
+                y: 'difficulty',
+                x: 'all',
+                title: (v) =>
+                  `${v.sent} / ${v.all} (${Number(v.completion).toLocaleString(undefined, { style: 'percent' })})`,
+                fill: 'rgba(169, 169, 169, 0.5)',
+              }),
+              Plot.barX(data, {
+                y: 'difficulty',
+                x: 'sent',
+                title: (v) =>
+                  `${v.sent} / ${v.all} (${Number(v.completion).toLocaleString(undefined, { style: 'percent' })})`,
+                fill: 1,
+              }),
+            ],
+            y: { type: 'band', reverse: true },
+          }}
+        />
+      </p>
+    </article>
   );
 }
--- website/app/components/Timeline.tsx
+++ website/app/components/Timeline.tsx
@@ -4,18 +4,21 @@ import * as d3 from 'd3';
 
 export function Timeline({ data }) {
   return (
-    <PlotFigure
-      width={600}
-      options={{
-        width: 600,
-        figure: true,
-        color: { scheme: 'YlGn' },
-        style: {
-          fontFamily: 'Inter',
-          fontSize: 'var(--step--1)',
-        },
+    <article>
+      <h2>Timeline</h2>
+      <p>
+        <PlotFigure
+          width={600}
+          options={{
+            width: 600,
+            figure: true,
+            color: { scheme: 'YlGn' },
+            style: {
+              fontFamily: 'Inter',
+              fontSize: 'var(--step--1)',
+            },
 
-        /*
+            /*
         x: {
           tickFormat: (d) =>
             Intl.DateTimeFormat('en', { month: 'long' }).format(
@@ -55,31 +58,37 @@ export function Timeline({ data }) {
         ],
         */
 
-        marginBottom: 50,
-        marks: [
-          Plot.barX(data, {
-            interval: 'week',
-            x: 'date',
-            fill: 'totalNumberDifficulty',
-            title: (d) =>
-              Object.entries(
-                d.difficulty.reduce((acc, grade) => {
-                  if (!acc[grade]) {
-                    acc[grade] = 0;
-                  }
-                  acc[grade] += 1;
-                  return acc;
-                }, {}),
-              )
-                .toSorted((a, b) => a[0].localeCompare(b[0]))
-                .reduce(
-                  (acc, ascents) => [...acc, `${ascents[0]} x${ascents[1]}`],
-                  [] as string[],
-                )
-                .join('; '),
-          }),
-        ],
-      }}
-    />
+            marginBottom: 50,
+            marks: [
+              Plot.barX(data, {
+                interval: 'week',
+                x: 'date',
+                fill: 'totalNumberDifficulty',
+                title: (d) =>
+                  Object.entries(
+                    d.difficulty.reduce((acc, grade) => {
+                      if (!acc[grade]) {
+                        <h2>Bar</h2>;
+                        acc[grade] = 0;
+                      }
+                      acc[grade] += 1;
+                      return acc;
+                    }, {}),
+                  )
+                    .toSorted((a, b) => a[0].localeCompare(b[0]))
+                    .reduce(
+                      (acc, ascents) => [
+                        ...acc,
+                        `${ascents[0]} x${ascents[1]}`,
+                      ],
+                      [] as string[],
+                    )
+                    .join('; '),
+              }),
+            ],
+          }}
+        />
+      </p>
+    </article>
   );
 }
